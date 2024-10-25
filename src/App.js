ecc38ad2b03be14b5b516216c698a062a7af9b29
daniel <dadapetr@gmail.com>
2024-10-25 14:14:35 +0200

    new App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import VideosPage from './pages/VideosPage';
+import ProfilePage from './pages/ProfilePage';
+import ShopPage from './pages/ShopPage';
+import AboutPage from './pages/AboutPage';
+import VideoDetailPage from './pages/VideoDetailPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/videos" element={<VideosPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/shop" element={<ShopPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/video/:videoId" element={<VideoDetailPage />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
   );
 }
 
